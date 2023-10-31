83960e3af428e21dfcc88a9a77b2ce5f8c9d6b18
nilesh0700 <nileshthengde0700@gmail.com>
2023-10-31 22:30:06 +0530

    final commit

--- index.js
+++ index.js
@@ -4,18 +4,30 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
-
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
+
 
 if(leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage;
-    renderLeads();
+    render(myLeads);
 }
+
+tabBtn.addEventListener("click", function(){
+
+    chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    })
+})
+
 
 deleteBtn.addEventListener("dblclick", function(){
     localStorage.clear();
     myLeads = [];
-    renderLeads();
+    render(myLeads);
 })
 
 inputBtn.addEventListener("click", function(){
@@ -23,17 +35,17 @@ inputBtn.addEventListener("click", function(){
     inputEl.value = "";
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
 
-   renderLeads();
+   render(myLeads);
 })
 
 
-function renderLeads(){
+function render(leads){
     let listItems = "";
     
-    for(let i = 0; i < myLeads.length; i++){
+    for(let i = 0; i < leads.length; i++){
         listItems += `
             <li>
-                <a href = '${myLeads[i]}'>${myLeads[i]}
+                <a href = '${leads[i]}'>${leads[i]}
                 </a>
             </li>
         `
