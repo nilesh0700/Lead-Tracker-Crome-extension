5bd6b3d19b79eca9fc419fc94a6a8004d97ecb68
nilesh0700 <nileshthengde0700@gmail.com>
2023-10-31 19:10:50 +0530

    delete button functionality

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage;
+    renderLeads();
+}
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear();
+    myLeads = [];
+    renderLeads();
+})
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
    renderLeads();
 })
 
@@ -16,12 +33,11 @@ function renderLeads(){
     for(let i = 0; i < myLeads.length; i++){
         listItems += `
             <li>
-                <a href = '${myLeads[i]}'>
-                ${myLeads[i]}
+                <a href = '${myLeads[i]}'>${myLeads[i]}
                 </a>
             </li>
         `
     }
     ulEl.innerHTML = listItems;
-    inputEl.value = "";
+   
 }
